a211e051fb52edc21081751ecfb05df38e379beb
Your Name <you@example.com>
2024-04-28 21:56:48 +0100

    first commit

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,7 +1,7 @@
 'use client'
 import { useEffect } from "react"
 import { useRouter } from "next/navigation"
-export default function(){
+export default function Login(){
     const router = useRouter();
     useEffect(()=>{
       router.push('/')
