112d7fc988ecaa3118dcab8242fc486cfc3f2d12
mukesh29kannan <152781611+mukesh29kannan@users.noreply.github.com>
2024-08-11 07:15:05 +0000

    fectcCHE

--- src/app/api/key/list/route.ts
+++ src/app/api/key/list/route.ts
@@ -1,15 +1,27 @@
-import {  Key } from "@/lib/models";
+import { Key } from "@/lib/models";
 import { connectToDb } from "@/lib/utils";
 import { NextResponse, NextRequest } from "next/server";
 
 export const GET = async (request: NextRequest) => {
-
     try {
-        connectToDb();
+        // Connect to the database
+        await connectToDb();
+
+        // Fetch data from the database
         const keys = await Key.find({});
-        return NextResponse.json({ status: true, data: keys });
+
+        // Create a response with cache control headers
+        const response = NextResponse.json({ status: true, data: keys });
+
+        // Set headers to prevent caching
+        response.headers.set('Cache-Control', 'no-store, no-cache, must-revalidate, proxy-revalidate');
+        response.headers.set('Pragma', 'no-cache');
+        response.headers.set('Expires', '0');
+        response.headers.set('Surrogate-Control', 'no-store');
+
+        return response;
     } catch (err) {
         console.log(err);
-        throw new Error("Something went wrong");
+        return NextResponse.json({ status: false, message: 'Something went wrong' });
     }
 };
