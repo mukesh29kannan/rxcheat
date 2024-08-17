dc342b661b81dbe1c5c882065cab29de7c268ac0
mukesh29kannan <152781611+mukesh29kannan@users.noreply.github.com>
2024-08-17 10:47:28 +0000

    fixed for console

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -4,25 +4,20 @@ export default function Dashboard() {
 
     return (
         <section className="page_404">
-	<div className="container">
-		<div className="row">	
-		<div className="col-sm-12 ">
-		<div className="col-sm-10 col-sm-offset-1  text-center">
-		<div className="four_zero_four_bg">
-			<h1 className="text-center ">HEY</h1>
-		
-		
-		</div>
-		
-		<div className="contant_box_404">
-		<2 className="h2">
-		Nee en da inga vandha! 🤨
-		</2>
-	</div>
-		</div>
-		</div>
-		</div>
-	</div>
-</section>
+			<div className="container">
+				<div className="row">	
+					<div className="col-sm-12 ">
+						<div className="col-sm-10 col-sm-offset-1  text-center">
+							<div className="four_zero_four_bg">
+								<h1 className="text-center ">HEY</h1>
+							</div>
+							<div className="contant_box_404">
+								<h2 className="h2">Nee en da inga vandha! 🤨</h2>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
     )
 }
