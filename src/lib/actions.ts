8df4da2a6f3a96f1d66a60a2ad92984b2a5b50f3
mukesh29kannan <152781611+mukesh29kannan@users.noreply.github.com>
2024-08-17 14:34:23 +0000

    let check

--- src/lib/actions.ts
+++ src/lib/actions.ts
@@ -1,5 +1,5 @@
 'use server'
-import { signIn, signOut } from "../auth";
+import { signIn, signOut } from "@/app/api/auth/[...nextauth]";
 
 export const handleLogout = async () => {
     "use server";
