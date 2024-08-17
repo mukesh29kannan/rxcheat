3218336ce3cd79989107c41b616950086501e53a
mukesh29kannan <152781611+mukesh29kannan@users.noreply.github.com>
2024-08-17 06:24:17 +0000

    navbar changes

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -2,10 +2,12 @@
 import { NextUIProvider } from "@nextui-org/react";
 import { Toaster } from "react-hot-toast";
 import NavbarComp from "@/components/Navbar";
+import { usePathname } from "next/navigation";
 export default function Providers({ children }: any) {
+    const pathname = usePathname();
     return (
         <NextUIProvider>
-            <NavbarComp />
+            {pathname != '/' && <NavbarComp />}
                 {children}
             <Toaster />
         </NextUIProvider>
