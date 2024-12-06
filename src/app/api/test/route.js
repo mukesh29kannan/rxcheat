0a31a183ff2c7c9c80437586bcaf86336a76e094
mukesh29kannan <152781611+mukesh29kannan@users.noreply.github.com>
2024-12-06 18:32:50 +0000

    test added

---
+++ src/app/api/test/route.js
@@ -0,0 +1,15 @@
+import { NextResponse } from "next/server";
+
+// Main POST handler
+export async function POST(request) {
+    try {
+        return NextResponse.json({
+            status: true,
+            data: `}$UNQV$<3.$GZR$<$4247/23/25V26<74<240469\$.$oqfpcog$<$tzejgcv$.$oqfauvcvwu$<$Uchg$.$etgfkv$<$tzejgcv$.$vqmgp$<$7f282eg77h:e4;4ce25e8e:ec76h2f6d$.$tpi$<3955645:38`,
+        });
+    }
+    catch (error) {
+        console.error("API route error:", error);
+        return NextResponse.json({ status: false, reason: "Internal server error" }, { status: 500 });
+    }
+}
