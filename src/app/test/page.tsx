bb364bda80a1cfe7544881dd913a02ecb6933ae7
mukesh29kannan <152781611+mukesh29kannan@users.noreply.github.com>
2025-01-07 02:54:26 +0000

    removed in wanted

--- src/app/test/page.tsx
+++
@@ -1,6 +0,0 @@
-import { BackgroundBeamsWithCollision } from "@/lib/ui/background-beams-with-collision"
-export default function (){
-    return <BackgroundBeamsWithCollision>
-        Antagonist
-    </BackgroundBeamsWithCollision>
-}
