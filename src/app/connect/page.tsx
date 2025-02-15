50bfe9eeaaf17f50540972c0ec2b347d191e61a2
mukesh29kannan <152781611+mukesh29kannan@users.noreply.github.com>
2025-02-15 17:31:54 +0000

    check

--- src/app/connect/page.tsx
+++ src/app/connect/page.tsx
@@ -4,7 +4,6 @@ import { Vortex } from "@/components/ui/vortex";
 
 export default function Connect() {
   return (
-    <div className="w-[calc(100%-4rem)] mx-auto rounded-md  h-[30rem] overflow-hidden">
       <Vortex
         backgroundColor="black"
         className="flex items-center flex-col justify-center px-2 md:px-10 py-4 w-full h-full"
@@ -16,6 +15,5 @@ export default function Connect() {
           <a href="https://t.me/rxcheat_hacker">@iamhackerbgmi</a>
         </p>
       </Vortex>
-    </div>
   );
 }
