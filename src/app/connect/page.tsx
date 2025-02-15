6ff89b6368ca79e7372b6e8bc5719de0ffd665b7
mukesh29kannan <152781611+mukesh29kannan@users.noreply.github.com>
2025-02-15 17:34:13 +0000

    screen

--- src/app/connect/page.tsx
+++ src/app/connect/page.tsx
@@ -1,19 +1,23 @@
-"use client"
+"use client";
 import React from "react";
 import { Vortex } from "@/components/ui/vortex";
 
 export default function Connect() {
   return (
+    <div className="w-screen h-screen flex items-center justify-center">
       <Vortex
         backgroundColor="black"
-        className="flex items-center flex-col justify-center px-2 md:px-10 py-4 w-full h-full"
+        className="flex items-center flex-col justify-center w-full h-full"
       >
         <h2 className="text-white text-2xl md:text-6xl font-bold text-center">
           RX Cheat
         </h2>
         <p className="text-white text-sm md:text-2xl max-w-xl mt-6 text-center">
-          <a href="https://t.me/rxcheat_hacker">@iamhackerbgmi</a>
+          <a href="https://t.me/rxcheat_hacker" className="underline">
+            @iamhackerbgmi
+          </a>
         </p>
       </Vortex>
+    </div>
   );
 }
