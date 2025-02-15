05a0c539e29cef8edd24b7c6660db1fa77b1d21d
mukesh29kannan <152781611+mukesh29kannan@users.noreply.github.com>
2025-02-15 16:33:36 +0000

    added

--- src/auth.config.ts
+++ src/auth.config.ts
@@ -6,7 +6,7 @@ export const authConfig = {
   },
   callbacks: {
     authorized({ auth, request: { nextUrl } }) {
-      // Check if the user is authenticated
+      // Check if the user is authenticated 
       const isLoggedIn = !!auth?.user;
       const isOnLoginPage = nextUrl.pathname == '/' || nextUrl.pathname == '/connect';
       console.log("auth user",auth?.user)
