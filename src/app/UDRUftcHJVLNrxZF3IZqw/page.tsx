1d33e33485a0dc0d713a4116489ba8ad46d10fb5
mukesh29kannan <152781611+mukesh29kannan@users.noreply.github.com>
2025-06-28 14:38:44 +0530

    Update page.tsx

--- src/app/UDRUftcHJVLNrxZF3IZqw/page.tsx
+++ src/app/UDRUftcHJVLNrxZF3IZqw/page.tsx
@@ -2,7 +2,7 @@ import EncryptedTimeClient from './EncryptedTimeClient';
 import crypto from 'crypto';
 import { deflateSync} from 'zlib';
 
-function encryptServer(text) {
+function encryptServer(text:any) {
   const algorithm = 'aes-256-cbc';
   const secret = "your-secret-key-here"; // Change this or put in env
 
